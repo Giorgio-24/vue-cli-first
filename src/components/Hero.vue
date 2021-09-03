9d330cac40668e326220e28fc85d045178bf130c
Giorgio Andrei <cipuandrei@libero.it>
2021-09-03 18:13:43 +0200

    jumbotron-with-logo-added

---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <img src="@/assets/logo.png" alt="Vue-logo" />
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.jumbotron {
+  max-width: 400px;
+}
+</style>
